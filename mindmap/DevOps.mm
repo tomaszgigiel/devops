4278a79116d2725058f249a36af7d49ec08f917d
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2021-06-12 16:14:47 +0200

    mindmap

---
+++ mindmap/DevOps.mm
@@ -0,0 +1,8 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1623430578678" ID="ID_1815446884" MODIFIED="1623430589753" TEXT="DevOps">
+<node CREATED="1623430902176" ID="ID_737800970" MODIFIED="1623430957336" POSITION="right" TEXT="I. Przep&#x142;yw"/>
+<node CREATED="1623430929879" ID="ID_286340092" MODIFIED="1623431038312" POSITION="left" TEXT="II. Sprz&#x119;&#x17c;enie zwrotne"/>
+<node CREATED="1623430989739" ID="ID_1807982937" MODIFIED="1623430998855" POSITION="right" TEXT="III. Eksperymentowanie"/>
+</node>
+</map>
